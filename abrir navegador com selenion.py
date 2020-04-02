462689c9b46b468a69741536501245df5aa13954
João Paulo Antunes <48777730+JoaoPauloAntunes@users.noreply.github.com>
2020-04-01 22:58:38 -0300

    Add files via upload

---
+++ abrir navegador com selenion.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+driver = webdriver.Firefox(firefox_binary=binary, executable_path=r'C:\\geckodriver.exe')
+driver.get('https://www.google.com')
+input()
